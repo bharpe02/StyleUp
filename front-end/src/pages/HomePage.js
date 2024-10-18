0f5d647d34ec866202606b7009bccb1ecb6c32a5
Nicc-Nova <nnovack@calpoly.edu>
2024-10-18 11:49:59 -0700

    going throught the trenches

---
+++ front-end/src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../assets/stylesheets/HomePage.css';
+import LogoButton from '../components/LogoButton.js';
+import Sidebar from '../components/Sidebar.js';
+
+function HomePage() {
+  return (
+    <div>
+        <div>
+            <LogoButton />
+        </div>
+        <div id="SidebarDiv">
+            <Sidebar />
+            
+        </div>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ front-end/src/pages/OpeningPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//import '../assets/stylesheets/OpeningPage.css';
+import LogoButton from '../components/LogoButton.js';
+
+function OpeningPage() {
+  return (
+    <div>
+        <LogoButton />
+    </div>
+  );
+}
+
+export default OpeningPage;

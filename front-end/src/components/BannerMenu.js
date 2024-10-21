9e4d706ffe8a559ae26b9d07bd0c794de37c2b8f
acream01 <acream@calpoly.edu>
2024-10-21 01:24:49 -0700

    Banner Menu and Gradient Background components

--- front-end/src/components/BannerMenu.js
+++ front-end/src/components/BannerMenu.js
@@ -4,10 +4,12 @@ import logo from '../assets/images/LOGO.png'
 function BannerMenu() {
   return (
     <div>
-        <svg width = "100vw" height = "200">
-            <rect width = "100%" height = "100%" fill = '#f0f0f0'/>
+        {/**Container for the Banner Menu. 
+         * NEEDS LOGO RESIZED, LOG IN, SIGN IN, CONDITIONAL "STATE" FOR SIGNED IN USER */}
+        <svg width = "100vw" height = "10vh">
+            <rect width = "100%" height = "100%" fill = 'white' />
         </svg> 
-        
+            {/**link to the homepage through the logo*/}          
             <a href="HomePage" target="_self" rel="noopener noreferrer">
                     <img src={logo} alt={""} style={homeLogoStyle}
                     width = '262' height = '85'
@@ -22,7 +24,7 @@ const homeLogoStyle = {
     cursor: 'pointer',
     position: 'absolute', 
     top: "10px",
-    left: "50px"
+    left: "50px",
 }
 
 
--- front-end/src/components/GradientBackground.js
+++ front-end/src/components/GradientBackground.js
@@ -2,9 +2,9 @@ import React from 'react'
 import '../assets/stylesheets/GradientBackground.css'
 
 function GradientBackground(){
+    /**Gradiant Component from bottom right to top left.  */
     return(
         <div className='background-gradient'>
-
         </div>
     )
 }

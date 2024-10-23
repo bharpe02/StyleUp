f4622624608bddea65141a31e2c8f05770ddfd71
Mish0322 <mishabandi@gmail.com>
2024-10-23 03:41:07 -0700

    Created Survey pages 1 and 2 with some functionality but needs adjustment still

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,20 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import OpeningPage from "./pages/OpeningPage";
 import MyRoomsPage from "./pages/MyRoomsPage"
 import AddRoomPage from "./pages/AddRoomPage"
 import WishlistPage from "./pages/WishlistPage"
 import SurveyPage from "./pages/SurveyPage"
+import Layout from './components/Layout';
 
 function App() {
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          <Route index element= {<OpeningPage />} />
-          <Route path="HomePage" element= {<HomePage />}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<OpeningPage />} />
+          <Route path="HomePage" element={<HomePage />} />
           <Route path="MyRooms" element={<MyRoomsPage />} />
           <Route path="AddRoom" element={<AddRoomPage />} />
           <Route path="Wishlist" element={<WishlistPage />} />
---
+++ front-end/src/components/Question2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Question2.css';
+
+function Question2({ onNext, onPrevious }) {
+  return (
+    <div className="question-container">
+      <h2>Question 2</h2>
+      <p>What is your color preference?</p>
+      <div className="color-options">
+        {['#FF0000', '#FFA500', '#FFFF00', '#7FFF00', '#00FF00'].map((color) => (
+          <div key={color} className="color-option" style={{ backgroundColor: color }}></div>
+        ))}
+      </div>
+      
+    </div>
+  );
+}
+
+export default Question2;

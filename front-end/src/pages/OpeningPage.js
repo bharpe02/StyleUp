963cd07e79b47a2b106b12b9d5658e90b2f41145
Nicc-Nova <nnovack@calpoly.edu>
2024-10-27 10:31:49 -0700

    Opening Page done

--- front-end/src/pages/OpeningPage.js
+++ front-end/src/pages/OpeningPage.js
@@ -1,13 +1,25 @@
 import React from 'react'
-//import '../assets/stylesheets/OpeningPage.css';
+import '../assets/stylesheets/OpeningPage.css';
 import BannerMenu from '../components/BannerMenu.js'
 import GradientBackground from '../components/GradientBackground'
+import { Link, useNavigate } from "react-router-dom";
 
 function OpeningPage() {
   return (
-    <div >
+    <div class="all">
         <BannerMenu />
+        <div class="gradient">
         <GradientBackground />
+        </div>
+        <div class="GetStarted">
+          Get Your Personalized Recomendations
+        </div>
+        <div class="GetStarted2">
+          <Link to="/Survey">
+            <button className="start-button">
+              Start
+            </button>
+          </Link></div>
     </div>
   );
 }

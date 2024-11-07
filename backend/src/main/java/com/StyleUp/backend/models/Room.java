75925e115012462cb977f53120c731e30af2d16d
Nicc-Nova <nnovack@calpoly.edu>
2024-11-07 06:47:48 -0800

    DB updated for rooms, decorations plus other wip

--- backend/src/main/java/com/StyleUp/backend/models/Room.java
+++ backend/src/main/java/com/StyleUp/backend/models/Room.java
@@ -1,4 +1,23 @@
 package com.StyleUp.backend.models;
 
+import jakarta.persistence.*;
+import java.lang.*;
+import java.util.*;
+
+@Entity
+@Table (name = "Room")
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rm_id;
+
+    @Column
+    private String roomName;
+
+    @OneToMany (cascade = CascadeType.ALL)//declares that one room can have many decorations, updates everything when one is saved
+    /*The @JoinColumn annotation combined with a @OneToOne mapping indicates that a given column in the owner entity
+    refers to a primary key in the reference entity*/
+    @JoinColumn (name = "fk_rm_id", referencedColumnName = "rm_id")
+    private List<Decoration> decorations;
+
 }
--- db-scripts/initialize_database.sql
+++ db-scripts/initialize_database.sql
@@ -12,3 +12,16 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+-- Create rooms table
+CREATE TABLE IF NOT EXISTS rooms (
+    rm_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    roomName VARCHAR(255) NOT NULL
+    fk_usr_id BIGINT
+);
+-- Create decorations table
+CREATE TABLE IF NOT EXISTS rooms (
+    dec_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    searchLink VARCHAR(255) NOT NULL,
+    fk_rm_id BIGINT 
+);

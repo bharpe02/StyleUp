49aec8564565520079ec019f3e87e0b1fd900a89
Bryn Harper <bharpe02@calpoly.edu>
2024-11-10 06:42:36 -0800

    Editing question 2

--- front-end/src/pages/SurveyPage.js
+++ front-end/src/pages/SurveyPage.js
@@ -7,6 +7,7 @@ import '../assets/stylesheets/Question1.css';
 
 function SurveyPage() {
   const [currentQuestion, setCurrentQuestion] = useState(1);
+  const [responses, setResponses] = useState({});
   const navigate = useNavigate();
 
   const handleNext = () => {
@@ -21,20 +22,33 @@ function SurveyPage() {
     }
   };
 
+  const handleAnswer = (question, answer) => {
+    setResponses((prevResponses) => ({
+      ...prevResponses,
+      [question]: answer,
+    }));
+  };
+
   return (
     <div className="survey-page">
       <LogoButton />
       <div className="main-content">
-        {currentQuestion === 1 && <Question1 />}
-        {currentQuestion === 2 && <Question2 />}
+        {currentQuestion === 1 && <Question1 onAnswer={(answer) => handleAnswer('Question 1', answer)}/>}
+        {currentQuestion === 2 && <Question2 onAnswer={(answer) => handleAnswer('Question 2', answer)}/>}
         {/* Add more questions similarly */}
         <div className="nav-buttons">
-          <button className="back-button" onClick={handlePrevious}>
-            ←
-          </button>
-          <button className="next-button" onClick={handleNext} disabled={currentQuestion === 5}>
-            Next
-          </button>
+          <button className="back-button" onClick={handlePrevious}>←</button>
+          <button className="next-button" onClick={handleNext} disabled={currentQuestion === 5}>Next</button>
+        </div>
+        <div className="responses-summary">
+          <h3>Survey Summary</h3>
+          <ul>
+            {Object.entries(responses).map(([question, answer]) => (
+              <li key={question}>
+                <strong>{question}:</strong> {answer}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
